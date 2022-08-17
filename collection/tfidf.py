d769624fa7be26d22b1e303e9b39972795b02a6d
Lucas Mahle <lucassmahle@gmail.com>
2022-08-16 21:55:15 -0300

    feat: Added learnign by chunks Added get_document_topics function

--- collection/tfidf.py
+++ collection/tfidf.py
@@ -4,10 +4,12 @@ This module generate info in order to generate TF-IDF data
 from gensim.corpora import Dictionary
 from gensim.models import TfidfModel
 
-def generate_dictionary(input_collection: 'list[list]', no_below: 'float', no_above: 'float', keep_n: 'int') -> 'Dictionary':
-  collection = input_collection.copy()
-
+def generate_dictionary(current_dictionary = None, collection: 'list[list]' = [], no_below: 'float' = 0, no_above: 'float' = 0, keep_n: 'int' = 0) -> 'Dictionary':
   dictionary = Dictionary(collection)
+
+  # Merge current dictionary
+  if current_dictionary is not None:
+    dictionary.merge_with(current_dictionary)
 
   # Filter dictionary considering % of the word recorrency and most frequency words
   dictionary.filter_extremes(no_below=len(collection) * no_below, no_above=no_above, keep_n=keep_n)
@@ -18,4 +20,4 @@ def generate_bow(collection: 'list[list]', dictionary: 'Dictionary') -> 'list':
   return [dictionary.doc2bow(d) for d in collection]
 
 def generate_tfidf_model(corpus_bow: 'list', dictionary: 'Dictionary') -> 'TfidfModel':
-  return TfidfModel(corpus_bow, dictionary = dictionary)
+  return TfidfModel(corpus_bow, dictionary=dictionary)
