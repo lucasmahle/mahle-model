641a51fb7d918c2ffe445adc479bdff09394a581
Lucas Mahle <lucassmahle@gmail.com>
2024-09-18 15:08:35 -0300

    build: moving source to src folder

--- __init__.py
+++ __init__.py
@@ -1,1 +1,1 @@
-from .model.MahleModel import MahleModel
+from .src.model.MahleModel import MahleModel
--- setup.py
+++ setup.py
@@ -21,17 +21,13 @@ extra_packages = spacy_packages + gensim_packages + sklearn_packages
 
 dev_packages = extra_packages
 
-with open("README.md", "r") as fh:
-    long_description = fh.read()
-
 setup(
     name="mahlemodel",
     packages=find_packages(exclude=[]),
     version="1.0.0",
-    author="Lucas S. Mahle",
+    author="Lucas Mahle",
     author_email="lucassmahle@gmail.com",
-    description="MahleModel performs topic Modeling with TF-IDF and K-means.",
-    long_description=long_description,
+    description="MahleModel performs topic modeling with TF-IDF and K-means.",
     long_description_content_type="text/markdown",
     url="https://github.com/lucasmahle/mahle-model",
     project_urls={
